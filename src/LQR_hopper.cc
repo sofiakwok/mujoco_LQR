efd43e2f755279b8626c449c2f25062f85962677
sofiakwok <sofiakwok8@gmail.com>
2023-03-13 17:45:54 -0400

    K matrix is not right

--- src/LQR_hopper.cc
+++ src/LQR_hopper.cc
@@ -1,7 +1,8 @@
 #include <stdbool.h> //for bool
 #include <iostream>
 //#include<unistd.h> //for usleep
-//#include <math.h>
+#include <math.h>
+#include <cmath>
 
 #include <GLFW/glfw3.h>
 #include <mujoco/mujoco.h>
@@ -40,6 +41,9 @@ mjtNum previous_time = 0;
 double_t ctrl_update_freq = 100;
 mjtNum last_update = 0.0;
 mjtNum ctrl;
+
+// time-invariant K
+double K[3] = {0, 0, 0};
 
 // keyboard callback
 void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods)
@@ -106,25 +110,52 @@ void scroll(GLFWwindow* window, double xoffset, double yoffset)
     mjv_moveCamera(m, mjMOUSE_ZOOM, 0, -0.05*yoffset, &scn, &cam);
 }
 
+MatrixXd quat_to_euler(mjtNum quat[4]){
+    double x = quat[0];
+    double y = quat[1];
+    double z = quat[2];
+    double w = quat[3];
+    double t0 = 2.0 * (w * x + y * z);
+    double t1 = 1.0 - 2.0 * (x * x + y * y);
+    double roll_x = atan2(t0, t1);
+    
+    double t2 = +2.0 * (w * y - z * x);
+    if (t2 > 1){
+        t2 = 1;
+    } 
+    if (t2 < -1.0){
+        t2 = 1;
+    }
+    double pitch_y = asin(t2);
+    
+    double t3 = +2.0 * (w * z + x * y);
+    double t4 = +1.0 - 2.0 * (y * y + z * z);
+    double yaw_z = atan2(t3, t4);
+
+    Matrix<double, 3, 1> euler = {roll_x, pitch_y, yaw_z};
+    
+    return euler; //in radians
+}
+
 MatrixXd LQR_controller(const mjModel* m, mjData* d)
 {
     // update constants
     double g = 9.81;
     double mass = 1;
-    double rw_mass = 1;
+    double rw_mass = 0.5;
     double I_p = 2;
-    double I_rw = 2;
-    double L = 1;
+    double I_rw = 1;
+    double L = 3;
     double l = 0.75;
 
     double a = rw_mass*L*L + I_p; 
     //cout << "a: " << a << endl;
     double b = mass*l + rw_mass*L;
 
-    Matrix<double, 3, 3> A;
-    A(0, 1) = 1;
-    A(1, 0) = b*g/a;
-    A(2, 0) = -b*g/a;
+    Matrix3d A;
+    A << 0, 1, 0, 
+        b*g/a, 0, 0,
+        -b*g/a, 0, 0;
     //cout << "A: " << A << endl; 
     Matrix<double, 3, 3> A_T = A.transpose();
     Matrix<double, 3, 1> B = {{0}, {-1/a}, {(a + I_rw)/(a*I_rw)}};
@@ -135,25 +166,39 @@ MatrixXd LQR_controller(const mjModel* m, mjData* d)
     //not used at all
     Matrix<double, 3, 1> D = {0, 0, 0};
 
-    Matrix<double, 3, 3> Q;
-    Q = C_T * C;
+    MatrixXd Q = C_T * C;
+    Q(0, 0) = 100;
+    Q(1, 1) = 100;
+    Q(2, 2) = 100;
     //cout << "Q: " << Q << endl;
     //this is sus
-    double R = 1;
+    double R = 0.0001;
 
-    //backwards Ricatti recursion
     Matrix3d P = Q;
     MatrixXd K;
     Matrix3d Pn;
-    //change arbitary amount of timesteps here at some point
-    for (int i = 10; i > 0; i--){
-        K = 1/(R + B_T*P*B)*B_T*P*A;
-        //cout << "K: " << K << endl;
-        Pn = Q + A_T*P*(A - B*K);
-        //cout << "Pn: " << Pn << endl;
-        P = Pn;
+    Matrix3d P2; 
+
+    for (int ricatti = 2; ricatti < 100; ricatti++){
+        //backwards Ricatti recursion
+        //change arbitary amount of timesteps here at some point
+        for (int i = ricatti; i > 0; i--){
+            // not using inv() here because R + B_T*P*B is a scalar
+            K = 1/(R + B_T*P*B)*B_T*P*A;
+            cout << "K: " <<K << endl;
+            Pn = Q + A_T*P*(A - B*K);
+            cout << "Pn: " << Pn << endl;
+            P2 = P;
+            P = Pn;
+        }
+
+        if ((P - P2).norm() <= 1e-10){
+            cout << "iters: " << ricatti << endl;
+            cout << "K: " << K << endl;
+            return K; 
+        }
     }
-    //cout << "K: " << K << endl;
+    cout << "Did not converge" << endl;
     return K;
 }
 
@@ -171,16 +216,6 @@ void mycontroller(const mjModel* m, mjData* d)
     int actuator_no;
     int bodyid;
 
-    //0 = first leg joint
-    actuator_no = mj_name2id(m, mjOBJ_ACTUATOR, "q0");
-    // set_position_servo(m, actuator_no, 1000);
-    d->ctrl[actuator_no] = 0;
-
-    //1 = second leg joint
-    actuator_no = mj_name2id(m, mjOBJ_ACTUATOR, "q2");
-    // set_position_servo(m, actuator_no, 1000);
-    d->ctrl[actuator_no] = 0;
-
     //fix leg angles 
     int joint_leg0 = mj_name2id(m, mjOBJ_JOINT, "Joint 0");
     int act_leg0 = mj_name2id(m, mjOBJ_ACTUATOR, "q0");
@@ -189,44 +224,51 @@ void mycontroller(const mjModel* m, mjData* d)
     int act_leg2 = mj_name2id(m, mjOBJ_ACTUATOR, "q2");
     d->ctrl[act_leg2] = -2000*d->qpos[m->jnt_qposadr[joint_leg2]] - 5*d->qvel[m->jnt_dofadr[joint_leg2]];
 
-    //getting K matrix for LQR controls
-    MatrixXd controls;
-    controls = LQR_controller(m, d);
-
-    //converting K matrix to K double
-    double K[3];
-    Map<MatrixXd>(K, controls.rows(), controls.cols()) = controls;
-
     //getting current COM position and velocity
     //body name is not right, need to add a body at COM
     bodyid = mj_name2id(m, mjOBJ_BODY, "rwz");
-    int composadr = -1, comveladr = -1;
-    //7 number position data: gives current position in 3D followed by a unit quaternion
-    composadr = m->jnt_qposadr[m->body_jntadr[bodyid]];
-    mjtNum COM_pos = d->qpos[composadr];
-    //6 number velocity data - gives linear velocity followed by angular velocity
-    comveladr = m->jnt_dofadr[m->body_jntadr[bodyid]];
-    mjtNum COM_vel = d->qpos[comveladr];
-    cout << "com vel: " << COM_vel << endl;
+    //gives current body frame orientation as a quaternion in Cartesian coordinates
+    mjtNum com_pos[4];
+    mju_copy(com_pos, d->xquat + m->jnt_qposadr[m->body_jntadr[bodyid]], 4);
+    //converting to euler angles
+    MatrixXd euler_angles;
+    euler_angles = quat_to_euler(com_pos);
+    //velocity data - gives linear velocity followed by angular velocity (# of entries = # of DOF)
+    mjtNum com_vel[4];
+    mju_copy(com_vel, d->qvel + m->jnt_dofadr[m->body_jntadr[bodyid]], 4);
+
+    /*cout << "com pos (x): " << com_pos[0] << endl;
+    cout << "com pos (y): " << com_pos[1] << endl;
+    cout << "com vel (x): " << com_vel[0] << endl;
+    cout << "com vel (y): " << com_vel[1] << endl;*/
 
     //2 = reaction wheel 1 (x)
     actuator_no = mj_name2id(m, mjOBJ_ACTUATOR, "rw0");
-    mjtNum state[2*m->nq];
+    int body_rw0 = mj_name2id(m, mjOBJ_BODY, "rw0");
+    mjtNum state[3];
     int xveladr = -1;
-    xveladr = m->jnt_dofadr[m->body_jntadr[actuator_no]];
+    xveladr = m->jnt_dofadr[m->body_jntadr[body_rw0]];
     mjtNum xvel = d->qvel[xveladr];
-    state[0] = COM_pos;
-    state[1] = COM_vel;
+    //cout << "rw (x): " << xvel << endl;
+    state[0] = euler_angles(0);
+    state[1] = com_vel[0];
     state[2] = xvel;
-    cout << "state: " << state << endl;
     mjtNum ctrl = mju_dot(K, state, 1);
-    cout << "control (x): " << ctrl << endl;
+    //cout << "control (x): " << ctrl << endl;
     d->ctrl[actuator_no] = -ctrl;
 
-    //3 = reaction wheel 2 (y)
+    //3 = reaction wheel 2 (z)
     actuator_no = mj_name2id(m, mjOBJ_ACTUATOR, "rw1");
-    ctrl = mju_dot(K, state, 2*m->nq);
-    cout << "control (y): " << ctrl << endl;
+    int body_rw1 = mj_name2id(m, mjOBJ_BODY, "rw1");
+    int yveladr = -1;
+    yveladr = m->jnt_dofadr[m->body_jntadr[body_rw1]];
+    mjtNum yvel = d->qvel[yveladr];
+    //cout << "rw (z): " << yvel << endl;
+    state[0] = euler_angles(2);
+    state[1] = com_vel[2];
+    state[2] = yvel;
+    ctrl = mju_dot(K, state, 1);
+    //cout << "control (y): " << ctrl << endl;
     d->ctrl[actuator_no] = -ctrl;
 }
 
@@ -284,6 +326,12 @@ int main(int argc, const char** argv)
     cam.lookat[2] = arr_view[5];
 
     d->qpos[0]=1.57; //pi/2
+    //getting K matrix for LQR controls
+    MatrixXd controls;
+    controls = LQR_controller(m, d);
+
+    //converting K matrix to K double
+    Map<MatrixXd>(K, controls.rows(), controls.cols()) = controls;
     mjcb_control = mycontroller;
 
     // use the first while condition if you want to simulate for a period.
