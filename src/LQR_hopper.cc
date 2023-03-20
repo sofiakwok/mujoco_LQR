881244ec943bc968dbcc97480447c40b2f9f02ee
sofiakwok <sofiakwok8@gmail.com>
2023-03-20 17:27:51 -0400

    now works with just current COM quaternion

--- src/LQR_hopper.cc
+++ src/LQR_hopper.cc
@@ -220,6 +220,7 @@ void mycontroller(const mjModel* m, mjData* d)
     //gives current body frame orientation as a quaternion in Cartesian coordinates
     mjtNum com_pos[4];
     mju_copy(com_pos, d->xquat + m->jnt_qposadr[m->body_jntadr[bodyid]], 4);
+    //making body frame orientation in world frame (just rearranges axes)
     mjtNum base_q[4];
     base_q[0] = 0;
     base_q[1] = 0;
@@ -227,29 +228,30 @@ void mycontroller(const mjModel* m, mjData* d)
     base_q[3] = 0;
     mjtNum trans_quat[4];
     mju_mulQuat(trans_quat, base_q, com_pos);
-    mjtNum com_location[3];
-    mju_copy(com_location, d->xpos + m->jnt_qposadr[m->body_jntadr[bodyid]], 3);
-    //defining reference quaternion (pointing straight up, at (0, 0) where foot contacts ground)
+    //defining reference quaternion (pointing straight up from (0, 0) where foot contacts ground)
     mjtNum quat_ref[4];
     quat_ref[0] = 0;
     quat_ref[1] = 0;
     quat_ref[2] = 1;
     quat_ref[3] = 0;
-    //finding difference between reference quaternion and current COM orientation, converting to 3D coordinates
+    //finding difference between reference quaternion and current COM orientation
+    mjtNum delta_q[4];
+    mju_mulQuat(delta_q, quat_ref, trans_quat);
+    //multiplying starting position difference between ref quaternion and current quaternion to find current COM
+    mjtNum ref_com[3];
+    ref_com[0] = 0;
+    ref_com[1] = 0;
+    ref_com[2] = 0.3;
     mjtNum delta_x[3];
-    mju_subQuat(delta_x, trans_quat, quat_ref);
-    //checking this is right
-    cout << "quat diff: " << delta_x[0] << " " << delta_x[1] << " " << delta_x[2] << endl;
-    cout << "quat tran: " << trans_quat[0] << " " << trans_quat[1] << " " << trans_quat[2] <<  " " << trans_quat[3] << endl;
-    cout << "com xyz: " << com_location[0] << " " << com_location[1] << " " << com_location[2] << endl;
+    mju_rotVecQuat(delta_x, ref_com, delta_q);
     //finding angle from z axis for x and y
     mjtNum angles[3];
-    angles[0] = atan(com_location[2]/(com_location[1]));
-    angles[1] = atan(com_location[0]/(com_location[1]));
+    angles[0] = atan(delta_x[0]/delta_x[2]);
+    angles[1] = atan(delta_x[1]/delta_x[2]);
     angles[2] = 0;
     cout << "angle (x): " << angles[0] << endl;
     cout << "angle (y): " << angles[1] << endl;
-    //transforming into reaction wheel frames
+    //transforming into reaction wheel frames (need to take rotation from x and y world frame axes into account)
     mjtNum reaction_angles[3];
     mjtNum rotation_matrix[9];
     mju_zero(rotation_matrix, 9);
@@ -261,20 +263,19 @@ void mycontroller(const mjModel* m, mjData* d)
     rotation_matrix[4] = cos(theta_rot - M_PI/4);
     mju_rotVecMat(reaction_angles, angles, rotation_matrix);
     //velocity data - gives rotational velocity followed by translational velocity (6x1)
-    //finding COM velocity
     mjtNum com_vel[6];
     mju_copy(com_vel, d->cvel + m->jnt_qposadr[m->body_jntadr[bodyid]], 6);
 
-    cout << "rw (x): " << reaction_angles[0] << endl;
+    /*cout << "rw (x): " << reaction_angles[0] << endl;
     cout << "rw (y): " << reaction_angles[1] << endl;
-    /*cout << "com (x): " << com_pos[0] << endl;
+    cout << "com (x): " << com_pos[0] << endl;
     cout << "com (y): " << com_pos[1] << endl;
     cout << "com (z): " << com_pos[2] << endl;
     cout << "com (w): " << com_pos[3] << endl;
     cout << "delta (x): " << delta_x[0] << endl;
     cout << "delta (y): " << delta_x[1] << endl;
-    cout << "delta (z): " << delta_x[2] << endl;*/
-    /*cout << "com vel (x): " << com_vel[3] << endl;
+    cout << "delta (z): " << delta_x[2] << endl;
+    cout << "com vel (x): " << com_vel[3] << endl;
     cout << "com vel (y): " << com_vel[4] << endl;
     cout << "com vel (z): " << com_vel[5] << endl;*/
 
